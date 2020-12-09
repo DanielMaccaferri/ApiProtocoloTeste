a2608edfd1faf2498bf74683eff0d23ec49753f4
danielmaccaferri <danmaccaferri@hotmail.com>
2020-12-09 17:36:22 -0300

    atualização feature

--- src/main/java/br/com/protocoloapi/steps/ProtocoloapiSteps.java
+++ src/main/java/br/com/protocoloapi/steps/ProtocoloapiSteps.java
@@ -16,22 +16,55 @@ public class ProtocoloapiSteps {
 	Resposta validarAssercao = new Resposta();
 
 	@Dado("^que eu realize uma requisicao na API para criar um protocolo  \"([^\"]*)\"  \"([^\"]*)\"  \"([^\"]*)\"$")
-	public void que_eu_realize_uma_requisicao_na_API_para_criar_um_protocolo(String conID, String mediaId, String cpf)
-			throws Throwable {
+	public void que_eu_realize_uma_requisicao_na_API_para_criar_um_protocolo_de_midia_Chat(String conID, String mediaId,
+			String cpf) throws Throwable {
 		protocolo.setConID(conID);
 		protocolo.setMediaId(mediaId);
 		protocolo.setCpf(cpf);
 
 		String json = gson.toJson(protocolo);
 		verbos.postEndPoint("http://192.168.246.238:8076/next-central-protocol/v1/protocol/protocols", json);
-		
+
 	}
-	
+
 	@Dado("^que eu realize uma requisicao na API para criar um protocolo  \"([^\"]*)\"  \"([^\"]*)\"$")
-	public void que_eu_realize_uma_requisicao_na_API_para_criar_um_protocolo(String conID, String mediaId) throws Throwable {
+	public void que_eu_realize_uma_requisicao_na_API_para_criar_um_protocolo_de_midia_Chat_sem_cpf(String conID,
+			String mediaId) throws Throwable {
 		protocolo.setConID(conID);
 		protocolo.setMediaId(mediaId);
-	
+
+		String json = gson.toJson(protocolo);
+		verbos.postEndPoint("http://192.168.246.238:8076/next-central-protocol/v1/protocol/protocols", json);
+	}
+
+	@Dado("^que eu realize uma requisicao na API para criar um protocolo sem conID   \"([^\"]*)\" \"([^\"]*)\"$")
+	public void que_eu_realize_uma_requisicao_na_API_para_criar_um_protocolo_sem_conID(String mediaId, String cpf)
+			throws Throwable {
+		protocolo.setMediaId(mediaId);
+		protocolo.setCpf(cpf);
+
+		String json = gson.toJson(protocolo);
+		verbos.postEndPoint("http://192.168.246.238:8076/next-central-protocol/v1/protocol/protocols", json);
+	}
+
+	@Dado("^que eu realize uma requisicao na API para criar um protocolo de midia Voz  \"([^\"]*)\"  \"([^\"]*)\"  \"([^\"]*)\"$")
+	public void que_eu_realize_uma_requisicao_na_API_para_criar_um_protocolo_de_midia_Voz(String conID, String mediaId,
+			String cpf) throws Throwable {
+		protocolo.setConID(conID);
+		protocolo.setMediaId(mediaId);
+		protocolo.setCpf(cpf);
+
+		String json = gson.toJson(protocolo);
+		verbos.postEndPoint("http://192.168.246.238:8076/next-central-protocol/v1/protocol/protocols", json);
+
+	}
+
+	@Dado("^que eu realize uma requisicao na API para criar um protocolo de midia Voz sem cpf  \"([^\"]*)\"  \"([^\"]*)\"$")
+	public void que_eu_realize_uma_requisicao_na_API_para_criar_um_protocolo_de_midia_Voz_sem_cpf(String conID,
+			String mediaId) throws Throwable {
+		protocolo.setConID(conID);
+		protocolo.setMediaId(mediaId);
+
 		String json = gson.toJson(protocolo);
 		verbos.postEndPoint("http://192.168.246.238:8076/next-central-protocol/v1/protocol/protocols", json);
 	}
--- src/test/resources/br/com/protocoloapi/feature/GerarProtocolo.feature
+++ src/test/resources/br/com/protocoloapi/feature/GerarProtocolo.feature
@@ -7,7 +7,7 @@ Funcionalidade: Gerar Protocolo
                 de gerar um protocolo
                 
                 
-@gerarprotocolo @post 
+@gerarprotocolochat @post 
 Esquema do Cenario: Gerar protocolo de Chat
        Dado que eu realize uma requisicao na API para criar um protocolo  "<conID>"  "<mediaId>"  "<cpf>"  
        Entao a API me retorna o status code 200
@@ -17,12 +17,39 @@ Exemplos:
 |0      |   C      | 11100011133           |   
 
 
-@gerarprotocolosemchat @post 
+@gerarprotocolosemcpfchat @post 
 Esquema do Cenario: Gerar protocolo de Chat sem CPF
-       Dado que eu realize uma requisicao na API para criar um protocolo  "<conID>"  "<mediaId>" 
+       Dado que eu realize uma requisicao na API para criar um protocolo de midia Chat sem cpf  "<conID>"  "<mediaId>" 
        Entao a API me retorna o status code 200
       
 Exemplos:
-|conID  |mediaId   |
-|0      |    C     |  
+|conID     |mediaId          |
+|0         |    C            |  
+
   
+@gerarprotocolosemconId @post 
+Esquema do Cenario: Gerar protocolo de Chat sem conID
+       Dado que eu realize uma requisicao na API para criar um protocolo sem conID   "<mediaId>" "<cpf>" 
+       Entao a API me retorna o status code 500
+      
+Exemplos:
+|mediaId    | CPF             |
+|    C      |  11100011133    |
+    
+@gerarprotocolovoz @post 
+Esquema do Cenario: Gerar protocolo de Voz
+       Dado que eu realize uma requisicao na API para criar um protocolo de midia Voz  "<conID>"  "<mediaId>"  "<cpf>"  
+       Entao a API me retorna o status code 200
+      
+Exemplos:
+|conID  |mediaId   |CPF                    |
+|0      |   V      | 11100011133           |      
+
+@gerarprotocolovozsemcpf @post 
+Esquema do Cenario: Gerar protocolo de Voz sem CPF
+       Dado que eu realize uma requisicao na API para criar um protocolo de midia Voz sem cpf  "<conID>"  "<mediaId>" 
+       Entao a API me retorna o status code 200
+      
+Exemplos:
+|conID     |mediaId          |
+|0         |    C            |       
